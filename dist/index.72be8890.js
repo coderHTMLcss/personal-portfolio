27e80f07199c989ad92b7741c3ae2e4e7b78ba45
viacheslav <kabaniets1990@gmail.com>
2023-06-15 16:55:57 +0300

    add files

---
+++ dist/index.72be8890.js
@@ -0,0 +1,7 @@
+const btnActive = document.getElementById("btnToggle");
+const menu = document.getElementById("active");
+btnActive.addEventListener("click", ()=>{
+    menu.classList.toggle("active");
+});
+
+//# sourceMappingURL=index.72be8890.js.map
---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnActive = document.getElementById('btnToggle');
+const menu = document.getElementById('active');
+
+btnActive.addEventListener('click', () => {
+    menu.classList.toggle('active')
+});
+
